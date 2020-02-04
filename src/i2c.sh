6861bc66fe95e2d9f9782f30c2db511a29d049de
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-02-04 10:17:30 +0530

    Update i2c.sh

--- src/i2c.sh
+++ src/i2c.sh
@@ -2,25 +2,23 @@
 counter=1
 while [ $counter -le 10 ]
 do
-	MP=$(( $(i2cget -y -a 1 0x5d 0x1A)))
-	let "n1 = $(($MP*(2**14)))"
-	LP=$(( $(i2cget -y -a 1 0x5d 0x1B)))
+        i2cset -y -a 1 0x5b 0x41 0x87
+        i2cset -y -a 1 0x5b 0x42 0x00
+        i2cset -y -a 1 0x5b 0x43 0x00
+        i2cset -y -a 1 0x5b 0x44 0x00
+        i2cset -y -a 1 0x5b 0x45 0x00
+        i2cset -y -a 1 0x5b 0x46 0x03
+	MP=$(($(i2cget -y -a 1 0x5b 0x50)))
+	let "n0 = $(($MP*(2**14)))"
+	LP=$(( $(i2cget -y -a 1 0x5d 0x51)))
 	let "n2 = $(($LP*(2**6)))"
-	LSP=$(( $(i2cget -y -a 1 0x5d 0x1B)))
+	LSP=$(( $(i2cget -y -a 1 0x5d 0x52)))
 	let "n3 = $(($LSP))"
 	let "n4 = $n1+$n2+$n3"
 	let "p=($n4/2048)"
-	echo "Pressure : $p hPa"
-
-	MT=$(( $(i2cget -y -a 1 0x5d 0x1D)))
-	let "n5 = $(($MT*(2**8)))"
-	LT=$(( $(i2cget -y -a 1 0x5d 0x1E)))
-	let "n6 = $(($LT))"
-	let "n7 = $n5+$n6"
-	let "t=($n7/32)"
-	echo "Temperature: $t *c "
-	
+	echo "Heart Rate $p bpm"
+        i2cget -y -a 1 0x5b 0x58
+	i2cset -y -a 1 0x5b 0x40 0x80
 	sleep 3s
-
 	
 done
