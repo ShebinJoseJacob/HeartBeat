0818baad390f662701122208bdde112f30fcdde1
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-02-04 20:42:14 +0530

    Update hb.sh

--- bin/hb.sh
+++ bin/hb.sh
@@ -8,17 +8,21 @@ do
         i2cset -y -a 1 0x5b 0x44 0x00
         i2cset -y -a 1 0x5b 0x45 0x00
         i2cset -y -a 1 0x5b 0x46 0x03
-	MP=$(($(i2cget -y -a 1 0x5b 0x50)))
-	let "n0 = $(($MP*(2**14)))"
-	LP=$(( $(i2cget -y -a 1 0x5d 0x51)))
-	let "n2 = $(($LP*(2**6)))"
-	LSP=$(( $(i2cget -y -a 1 0x5d 0x52)))
-	let "n3 = $(($LSP))"
-	let "n4 = $n1+$n2+$n3"
-	let "p=($n4/2048)"
-	echo "Heart Rate $p bpm"
-        i2cget -y -a 1 0x5b 0x58
+	
+	MH=$(($(i2cget -y -a 1 0x5b 0x50)))
+	let "n0 = $(($MH*(2**14)))"
+	LH=$(( $(i2cget -y -a 1 0x5b 0x51)))
+	let "n2 = $(($LH*(2**8)))"
+	LSH=$(( $(i2cget -y -a 1 0x5b 0x52)))
+	let "n3 = $(($LSH))"
+	LSSH=$(( $(i2cget -y -a 1 0x5b 0x53)))
+	let "n4 = $(($LSSH))"
+	let "n5 = $n1+$n2+$n3+$n4"
+	let "h=($n5/2048)"
+	echo "Heart Rate $h bpm"
+
 	i2cset -y -a 1 0x5b 0x40 0x80
 	sleep 3s
 	
 done
+
