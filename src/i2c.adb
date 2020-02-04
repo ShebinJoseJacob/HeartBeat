ea79ccfd8dcdcc06c4352031ca7fdb5b1c9b5097
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-02-04 10:19:34 +0530

    Update i2c.adb

--- src/i2c.adb
+++ src/i2c.adb
@@ -7,32 +7,7 @@ with Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO;
 
 package body i2c is
-    R,V1: Integer;
-    W1: String:="i2cset -y -a 1 0x5b 0x41 0x87";
-    W2: String:="i2cset -y -a 1 0x5b 0x42 0x00";
-    W3: String:="i2cset -y -a 1 0x5b 0x43 0x00";
-    W4: String:="i2cset -y -a 1 0x5b 0x44 0x00";
-    W5: String:="i2cset -y -a 1 0x5b 0x45 0x00";
-    W6: String:="i2cset -y -a 1 0x5b 0x46 0x03";
-    
-
-    procedure write is
-        function System (Cmd : String) return Integer is
-            function C_System (S : Interfaces.C.char_array) return Integer;
-        pragma Import (C, C_System, "system");
-        begin
-            return C_System (Interfaces.C.To_C (Cmd));        
-        end System;
-        pragma Inline (System);
-    begin
-    R := System (W1);
-    DELAY 0.5;
-    R := System (W2);
-    R := System (W3);
-    R := System (W4);
-    R := System (W5);
-    R := System (W6);
-    end write;
+    V1: Integer;
     
     procedure read  is
           function System (Cmd : String) return Integer is
